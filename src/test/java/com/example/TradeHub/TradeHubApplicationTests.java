6f11927384bfa07274e5bcfd3812bb58090f62a2
sabexzero <sabexqe@yandex.ru>
2024-03-28 23:45:44 +0300

    initial

---
+++ src/test/java/com/example/TradeHub/TradeHubApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.TradeHub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TradeHubApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
