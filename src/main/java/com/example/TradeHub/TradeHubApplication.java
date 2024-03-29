3d18389a6c857d9eee6ee7223e6fcf3c7cf311e1
sabexzero <sabexqe@yandex.ru>
2024-03-29 03:00:31 +0300

    Added TODO'S

--- src/main/java/com/example/TradeHub/TradeHubApplication.java
+++ src/main/java/com/example/TradeHub/TradeHubApplication.java
@@ -5,7 +5,27 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class TradeHubApplication {
-
+	//TODO_SECTION
+	
+	//TODO: Implement user repository
+	//	labels: enhancement
+	
+	//TODO: Implement cryptocurrency repository
+	//	labels: enhancement
+	
+	//TODO: Implement currency repository
+	//	labels: enhancement
+	
+	//TODO: Implement luquibase
+	// labels: enhancement
+	
+	//TODO: Write tests for repositories
+	// labels: tests
+	
+	//TODO: Write SwaggerInfo and JavaDoc for domain and repository
+	// labels: documentation
+	
+	// END TODO_SECTION
 	public static void main(String[] args) {
 		SpringApplication.run(TradeHubApplication.class, args);
 	}
